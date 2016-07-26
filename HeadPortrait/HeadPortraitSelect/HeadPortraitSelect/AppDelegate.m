92c57f7c9cf4c3a24e5398a0d02eed93fde08aae
高崇彬 <769728828@qq.com>
2016-07-26 16:16:25 +0800

    no message

--- HeadPortrait/HeadPortraitSelect/HeadPortraitSelect/AppDelegate.m
+++ HeadPortrait/HeadPortraitSelect/HeadPortraitSelect/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-//    application hot-fix
+//    application hot-fix2014
     //ceshi0004444
     
     return YES;
