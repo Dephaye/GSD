3104f077b9d8362d03d159ca2f449e0dd0afd3be
高崇彬 <769728828@qq.com>
2016-07-26 13:45:11 +0800

    hot-fix commit 1

--- HeadPortrait/HeadPortraitSelect/HeadPortraitSelect/AppDelegate.m
+++ HeadPortrait/HeadPortraitSelect/HeadPortraitSelect/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    
+//    application hot-fix
     //ceshi0004444
     
     return YES;
